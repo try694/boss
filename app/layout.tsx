41efd7ecab582742a93d0d3342d4d9d0ce77420c
tryhard0000 <bhustle443@gmail.com>
2025-04-19 22:21:22 +0300

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
+// app/layout.tsx
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata: Metadata = {
-  title: "Create",
-  description: "Generatep",
+  title: "Amazing Trading",
+  description: `Discover the future of arbitrage. Seamlessly optimize your trades and get ahead with real‑time tools built for modern finance.`,
+  icons: {
+    icon: "/favicon.ico",            // your favicon in public/favicon.ico
+    shortcut: "/favicon.ico",        // legacy fallback
+    apple: "/apple-touch-icon.png",  // optional iOS icon
+  },
 };
 
 export default async function RootLayout({
@@ -17,14 +23,15 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await auth();
+
   return (
     <SessionProvider session={session}>
-    <html lang="en">
-      <body className="bg-gray-900 poppins-regular">
-        {children}
-        <ToastContainer autoClose={2000} />
-      </body>
-    </html>
+      <html lang="en">
+        <body className="bg-gray-900 poppins-regular">
+          {children}
+          <ToastContainer autoClose={2000} />
+        </body>
+      </html>
     </SessionProvider>
   );
 }
